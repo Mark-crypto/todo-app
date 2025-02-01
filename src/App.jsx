02a841aa8d1bc2083e59d7a63acec933b9156e73
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-02-01 22:14:01 +0300

    starting a new todo project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h5>To Do App</h5>
+    </>
+  );
+}
+
+export default App;
