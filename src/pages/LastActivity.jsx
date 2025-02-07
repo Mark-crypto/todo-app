f931b927f579c8f3bf1d84b9435a276f6082eb49
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-02-07 15:58:56 +0300

    working on last activity and messages page

--- src/pages/LastActivity.jsx
+++ src/pages/LastActivity.jsx
@@ -6,6 +6,25 @@ const LastActivity = () => {
   return (
     <>
       <Navbar />
+      <div
+        className="lastactivity"
+        style={{ backgroundColor: "#f4f4f4", height: "100vh", padding: "20px" }}
+      >
+        <div className="activity">
+          <h6 style={{ textDecoration: "underline" }}>Completed Activity</h6>
+          <p>
+            You marked client review & feedback as completed at{" "}
+            <span style={{ color: "black" }}>12:00PM today</span>.
+          </p>
+        </div>
+        <div className="activity">
+          <h6 style={{ textDecoration: "underline" }}>Deleted Activity</h6>
+          <p>
+            You deleted your planned shopping experience at{" "}
+            <span style={{ color: "black" }}>10:00AM today</span>.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -6,6 +6,21 @@ const Message = () => {
   return (
     <>
       <Navbar />
+      <div
+        className="message"
+        style={{ backgroundColor: "#f4f4f4", height: "100vh", padding: "20px" }}
+      >
+        <div className="notification">
+          <h5 style={{ textDecoration: "underline" }}>Notification</h5>
+          <div className="notification-content" style={{ marginTop: "-10px" }}>
+            <h6>New Feature Alert!!</h6>
+            <p>
+              We have released the new AI. To help you plan out your day
+              sufficiently.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
